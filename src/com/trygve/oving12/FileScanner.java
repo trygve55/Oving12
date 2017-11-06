c4ab6762c06d59df42cf95dd0b50f1e915bb4be7
Andrz Cabal <andrzejc@stud.ntnu.no>
2017-11-06 15:08:02 +0100

    Read files.

---
+++ src/com/trygve/oving12/FileScanner.java
@@ -0,0 +1,13 @@
+package shrink;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileScanner {
+	// Read every byte of the file.
+	public static byte[] loadFile(String path) {
+		try {return Files.readAllBytes(Paths.get(path));}
+		catch (IOException e) {return null;}
+	}
+}
